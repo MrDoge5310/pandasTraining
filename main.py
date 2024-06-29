9ed678b8d56ccd4e1d285d5f05d11ed578d6acbc
LOGIKA <danilltrotsenko2002@gmail.com>
2024-06-29 14:33:25 +0300

    1

---
+++ main.py
@@ -0,0 +1,17 @@
+import pandas
+
+dataframe = pandas.read_csv("GoogleApps.csv")
+
+avg_price = round(dataframe[dataframe['Price'] > 0]['Price'].mean(), 2)
+print("Average price:", avg_price, "$")
+
+max_price = dataframe['Price'].max()
+print("Max price:", max_price, "$")
+
+min_price = dataframe['Price'].min()
+print("Min price:", min_price, "$")
+
+free_amount = dataframe[dataframe['Price'] == 0.0]['Price'].count()
+total_amount = dataframe['Price'].count()
+free_apps_procente = round(free_amount * 100 / total_amount, 2)
+print(str(free_apps_procente) + "%", "- free apps")
